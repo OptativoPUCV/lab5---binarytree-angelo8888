36d9622aa7b790513211fe5baaeea53d3ac95b1b
angelo8888 <142196931+angelo8888@users.noreply.github.com>
2023-09-15 00:02:04 -0300

    Update treemap.c

--- treemap.c
+++ treemap.c
@@ -36,12 +36,14 @@ TreeNode * createTreeNode(void* key, void * value) {
     return new;
 }
 
+
 TreeMap * createTreeMap(int (*lower_than) (void* key1, void* key2)) {
-
-    //new->lower_than = lower_than;
-    return NULL;
+    TreeMap* map = (TreeMap*)malloc(sizeof(TreeMap));
+    map->root = NULL;
+    map->current = NULL;
+    map->lower_than = lower_than;
+    return map;
 }
-
 
 void insertTreeMap(TreeMap * tree, void* key, void * value) {
 
